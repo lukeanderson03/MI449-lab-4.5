ec76108c48b33306abdb8c3eda4d65cf0ea013a5
Luke Anderson <ande2594@msu.edu>
2025-02-05 22:18:09 -0500

    Added javascript shortcode, liquid layout, and css

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 
 // The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+
+  eleventyConfig.addShortcode("planet-cards", function (group, name, astroUnit) {
+    return `<div class="${group}">
+    <div id="${name}"></div>
+    <h2>${name}</h2>
+    <h3>${astroUnit}</h3>
+    </div>`
+  })
 };
